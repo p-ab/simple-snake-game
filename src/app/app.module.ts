0c6ebee426367a23c8602cbfc73ad4c8de211061
P. Abramov <pasha.abramov.92@gmail.com>
2020-07-20 13:30:25 +0300

    grid version

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,11 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import { GameScreenComponent } from './game-screen/game-screen.component';
 import { GridLayoutComponent } from './grid-layout/grid-layout.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    GameScreenComponent,
     GridLayoutComponent
   ],
   imports: [
